d5a38908d711d6460abe39a8b87cf6061fd97574
Daniil <dan.bardyuk@yandex.ru>
2022-10-28 11:14:42 +0300

    fix: add handler for all selctor

--- script.js
+++ script.js
@@ -2,96 +2,102 @@ window.createCustomSelector = function (
   selector,
   { label = null, multiple = false, input = false }
 ) {
-  const select = document.querySelector(selector);
-  const options = select.querySelectorAll("option");
-  const selectorWrapper = createElement("div", "selectorWrapper");
-  selectorWrapper.classList.add(`${selector.slice(1)}`);
-  const listWrapper = createElement("div", "listWrapper");
+  const select = document.querySelectorAll(selector);
+  select.forEach((el) => {
+    const options = el.querySelectorAll("option");
+    const selectorWrapper = createElement("div", "selectorWrapper");
+    const listWrapper = createElement("div", "listWrapper");
+    const customClass = `MyClass-${Math.floor(Math.random() * 1000)}`;
+    selectorWrapper.classList.add(customClass);
 
-  const inputField = createElement("input", "selectSearch");
-  if (input) {
-    listWrapper.append(inputField);
-  }
+    const [optionList, list] = renderList(options);
+    listWrapper.append(list);
 
-  const [optionList, list] = renderList(options);
-  listWrapper.append(list);
+    if (input) {
+      const inputField = createElement("input", "selectSearch");
+      inputField.addEventListener("input", (e) => {
+        const target = e.target;
+        optionList.forEach((el) => {
+          el.classList.remove("hidden");
+          if (!el.textContent.toLowerCase().includes(target.value))
+            el.classList.add("hidden");
+        });
+      });
+      listWrapper.prepend(inputField);
+    }
 
-  inputField.addEventListener("input", (e) => {
-    const target = e.target;
-    optionList.forEach((el) => {
-      el.classList.remove("hidden");
-      el.textContent.toLowerCase().includes(target.value)
-        ? el.classList.remove("hidden")
-        : el.classList.add("hidden");
+    const selectBlock = createElement("div", "selectBlock");
+    const selectHeader = createElement("div", "selectHeader");
+    const btn = createElement("button", "chevron");
+    const selectorContent = createElement(
+      "div",
+      "selectContent",
+      "Выберите элемент"
+    );
+    selectHeader.append(selectorContent, btn);
+
+    const $label = findLabel(el.getAttribute("id"));
+    const actualLabel = label ?? $label?.textContent;
+    if (actualLabel) {
+      const selectLabel = createElement("label", null, actualLabel);
+      if ($label) $label.style.display = "none";
+      selectBlock.append(selectLabel);
+    }
+
+    selectorWrapper.append(selectHeader, listWrapper);
+    selectBlock.append(selectorWrapper);
+    el.after(selectBlock);
+    el.style = `display:none`;
+
+    selectHeader.addEventListener("click", (e) => {
+      selectBlock.classList.toggle("My-custom-select");
+      const offset =
+        document.documentElement.scrollHeight -
+          selectBlock.getBoundingClientRect().bottom <
+        300
+          ? listWrapper.clientHeight + selectBlock.clientHeight
+          : 0;
+
+      listWrapper.style.transform = `translateY(-${offset}px)`;
     });
-  });
-  const selectBlock = createElement("div", "selectBlock");
-  const selectHeader = createElement("div", "selectHeader");
-  const btn = createElement("button", "chevron");
-  const selectorContent = createElement(
-    "div",
-    "selectContent",
-    "Выберите элемент"
-  );
-  selectHeader.append(selectorContent, btn);
 
-  const $label = findLabel(selector);
-  const actualLabel = label ?? $label?.textContent;
-  if (actualLabel) {
-    const selectLabel = createElement("label", null, actualLabel);
-    if ($label) $label.style.display = "none";
-    selectBlock.append(selectLabel);
-  }
+    let selectOption = [];
+    listWrapper.addEventListener("click", (e) => {
+      const target = e.target;
+      if (target.classList.contains("option")) {
+        if (multiple) {
+          target.classList.toggle("select");
 
-  selectorWrapper.append(selectHeader, listWrapper);
-  selectBlock.append(selectorWrapper);
-  select.after(selectBlock);
-  select.style = `display:none`;
+          selectOption.includes(target.textContent)
+            ? (selectOption = selectOption.filter(
+                (el) => el !== target.textContent
+              ))
+            : selectOption.push(target.textContent);
 
-  selectHeader.addEventListener("click", (e) => {
-    selectBlock.classList.toggle("isActive");
-    const offset =
-      document.documentElement.scrollHeight -
-        selectBlock.getBoundingClientRect().bottom <
-      300
-        ? listWrapper.clientHeight + selectBlock.clientHeight
-        : 0;
-
-    listWrapper.style.transform = `translateY(-${offset}px)`;
-  });
-
-  let arr = [];
-  listWrapper.addEventListener("click", (e) => {
-    const target = e.target;
-    if (target.classList.contains("option")) {
-      if (multiple) {
-        target.classList.toggle("select");
-
-        arr.includes(target.textContent)
-          ? (arr = arr.filter((el) => el !== target.textContent))
-          : arr.push(target.textContent);
-
-        selectorContent.innerHTML =
-          arr.length === 0 ? "Выберите элемент" : arr.join(" ");
-      } else {
-        optionList.forEach((el) => el.classList.remove("select"));
-        target.classList.add("select");
-        selectorContent.innerHTML = target.textContent;
-        selectBlock.classList.remove("isActive");
+          selectorContent.innerHTML =
+            selectOption.length === 0
+              ? "Выберите элемент"
+              : selectOption.join(" ");
+        } else {
+          optionList.forEach((el) => el.classList.remove("select"));
+          target.classList.add("select");
+          selectorContent.innerHTML = target.textContent;
+          selectBlock.classList.remove("My-custom-select");
+        }
       }
-    }
-  });
+    });
 
-  window.addEventListener("click", (e) => {
-    const target = e.target;
-    if (!target.closest(`.${selector.slice(1)}`)) {
-      selectBlock.classList.remove("isActive");
-    }
+    window.addEventListener("click", (e) => {
+      const target = e.target;
+      if (!target.closest(`.${customClass}`)) {
+        selectBlock.classList.remove("My-custom-select");
+      }
+    });
   });
 };
 
-function findLabel(selector) {
-  return document.querySelector(`label[for = "${selector.slice(1)}"]`);
+function findLabel(id) {
+  return document.querySelector(`label[for = "${id}"]`);
 }
 
 function createElement(tag, className = "", content = "") {
@@ -103,13 +109,14 @@ function createElement(tag, className = "", content = "") {
   return element;
 }
 
-function renderList(arr) {
+function renderList(nodeList) {
   const list = createElement("ul");
-  arr.forEach((elem) => {
+  const optionList = Array.from(nodeList).map((elem) => {
     const item = createElement("li", "option");
     item.setAttribute("value", elem.value);
     item.innerHTML = elem.textContent;
     list.append(item);
+    return item;
   });
-  return [list.querySelectorAll(".option"), list];
+  return [optionList, list];
 }
